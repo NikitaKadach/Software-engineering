ef25a59e11cbc29d4d987125df544984916648e0
Nikita Kadach <Kadach_NV_25@student.grsu.by>
2025-10-24 12:00:34 +0300

    Update a3.py

--- lab_3/a1/a3/a3/a3.py
+++ lab_3/a1/a3/a3/a3.py
@@ -5,14 +5,15 @@ if curr >= prev:
 else:
     used = (10000 - prev) + curr
 if used <= 300:
-    pay = 21.5
+    pay = 21
 elif used <= 600:
-    pay = 21.5 + (used - 300) * 0.06
+    pay = 21 + (used - 300) * 0.06
 elif used <= 800:
-    pay = 21.5 + 300 * 0.06 + (used - 600) * 0.04
+    pay = 21 + 300 * 0.06 + (used - 600) * 0.04
 else:
-    pay = 21.5 + 300 * 0.06 + 200 * 0.04 + (used - 800) * 0.025
+    pay = 21 + 300 * 0.06 + 200 * 0.04 + (used - 800) * 0.025
 avg_price = pay / used
 
 print(f"{'Previous':>8} {'Current':>6} {'Used':>6} {'To pay':>7} {'Avg. price m^3':>13}")
 print(f"{prev:>8} {curr:>6} {used:>6} {pay:>7.2f} {avg_price:>12.2f}")
+
