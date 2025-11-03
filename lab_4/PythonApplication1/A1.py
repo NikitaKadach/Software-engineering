ce8881eafb5d5849babdda1e43ac49b754ea358e
Nikita Kadach <Kadach_NV_25@student.grsu.by>
2025-11-03 20:44:43 +0300

    Add files via upload

---
+++ lab_4/PythonApplication1/A1.py
@@ -0,0 +1,11 @@
+x = int(input("x = "))
+y = int(input("y = "))
+for i in range(y):
+    print("#" * x)
+for i in range(y):
+    print("#" * (i + 1))
+for i in range(y):
+    if i == 0 or i == y - 1:
+        print("#" * x)
+    else:
+        print("#" + " " * (x - 2) + "#")
